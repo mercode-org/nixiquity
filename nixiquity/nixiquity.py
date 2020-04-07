26386c5067dde3219fa7b51801eb2312731ecfd2
Maciej Krüger <mkg20001@gmail.com>
2020-04-07 04:51:49 +0200

    feat: implement some ui stuff

--- nixiquity/nixiquity.py
+++ nixiquity/nixiquity.py
@@ -13,6 +13,20 @@ class Installer:
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file('%s/ubiquity.ui' % UIDIR)
+        self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("live_installer")
+        self.window.show()
+        self.quit_warning = self.builder.get_object("warning_dialog")
+
+    def on_quit_clicked(self, event):
+        self.quit_warning.show()
+
+    def on_quit_cancelled(self, event):
+        self.quit_warning.hide()
+
+    def quit_installer(self, event=None):
+        Gtk.main_quit()
 
     def loop(self):
         """Run the UI's main loop until it returns control to us."""
